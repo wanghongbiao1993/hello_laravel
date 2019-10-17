587c7637174235497031f661858d8394a6c26a5a
Assassin <wanghb@kaiwait.com>
2019-10-17 14:35:52 +0800

    命名路由

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,7 +4,7 @@
     <div class="jumbotron">
         <h1>Hello Laravel</h1>
         <p class="lead">
-            你现在所看到的是 <a href="https://kawait.herokuapp.com/">Laravel 入门教程</a> 的示例项目主页。
+            你现在所看到的是 <a href="{{ route('home') }}">Zealoter</a> 的项目主页。
         </p>
         <p>
             一切，将从这里开始。
