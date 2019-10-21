28d7bd305843e92137fc8cdfa6b60639c5ce5ddf
Assassin <wanghb@kaiwait.com>
2019-10-21 15:31:42 +0800

    用户微博列表

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -36,7 +36,11 @@ class UserController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+
+        $statuses = $user->statuses()
+            ->orderBy('created_at', 'desc')
+            ->paginate(30);
+        return view('users.show', compact('user','statuses'));
     }
 
     /**
