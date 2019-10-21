bb21e44db5217cab1a89ea7baded09a9e6904054
Assassin <wanghb@kaiwait.com>
2019-10-21 15:12:01 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 
     public function gravatar($size = '100')
     {
