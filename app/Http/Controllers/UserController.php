382c95222369e218f8d21ad15217d8173976cb64
Assassin <wanghb@kaiwait.com>
2019-10-21 13:55:13 +0800

    移除邮件发送的 from选项

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -99,13 +99,11 @@ class UserController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = 'wanghongbiao1993@gmail.com';
-        $name = '王洪彪';
         $to = $user->email;
         $subject = "感谢注册 Weibo 应用！请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject) {
+            $message->to($to)->subject($subject);
         });
     }
 
