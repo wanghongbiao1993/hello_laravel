0a764833969781a6ba4434ce0165c57c6fdcccd6
Assassin <wanghb@kaiwait.com>
2019-10-21 17:00:08 +0800

    粉丝列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,6 +39,18 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+    }
+
+    public function followings()
+    {
+        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+    }
+
+
     public function statuses()
     {
         return $this->hasMany(Status::class);
@@ -63,5 +75,27 @@ class User extends Authenticatable
     {
         return $this->statuses()
             ->orderBy('created_at', 'desc');
+    }
+
+
+    public function follow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    public function unfollow($user_ids)
+    {
+        if ( ! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    public function isFollowing($user_id)
+    {
+        return $this->followings->contains($user_id);
     }
 }
